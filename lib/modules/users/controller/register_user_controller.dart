7624224287311efbee0f0c1e3b796e8a07e06a51
Abner Silva <abner.computadores@outlook.com>
2020-09-29 10:32:40 -0300

    inicio do projeto e configuração de conexão com banco de dados Mysql

---
+++ lib/modules/users/controller/register_user_controller.dart
@@ -0,0 +1,35 @@
+import 'package:injectable/injectable.dart';
+import 'package:pizza_delivery_api/modules/users/controller/models/register_user_rq.dart';
+import 'package:pizza_delivery_api/modules/users/service/i_user_service.dart';
+import 'package:pizza_delivery_api/modules/users/view_models/register_user_input_model.dart';
+import 'package:pizza_delivery_api/pizza_delivery_api.dart';
+
+@Injectable()
+class RegisterUserController extends ResourceController {
+  final IUserService _service;
+
+  RegisterUserController(this._service);
+
+  @Operation.post()
+  Future<Response> registerUser(
+      @Bind.body() RegisterUserRq registerUserRq) async {
+    final registerInput = mapper(registerUserRq);
+
+    try {
+      await _service.registerUser(registerInput);
+      return Response.ok({'message': 'usuário criado com sucesso'});
+    } catch (e) {
+      print(e);
+      return Response.serverError(
+          body: {'message': 'Erro ao registrar novo usuário'});
+    }
+  }
+
+  RegisterUserInputModel mapper(RegisterUserRq registerUserRq) {
+    return RegisterUserInputModel(
+      name: registerUserRq.name,
+      email: registerUserRq.email,
+      password: registerUserRq.password,
+    );
+  }
+}
